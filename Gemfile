turbolinks/turbolinks

commit 81785f12e37d3d954e12a4df91c4d302c29afd05

CI: ✅

.blade.yml
@@ -13,3 +13,10 @@ build:
     - turbolinks.js
   path: dist
   js_compressor: uglifier
+
+plugins:
+  sauce_labs:
+    browsers:
+      Google Chrome:
+        os: Mac
+        version: -1

Gemfile
@@ -6,4 +6,7 @@ gem 'coffee-script'
 gem 'coffee-script-source', '~> 1.9.1'
 gem 'eco'
 gem 'uglifier'
-gem 'blade'
+
+gem 'blade', '~> 0.7.1'
+gem 'blade-sauce_labs_plugin', '~> 0.7.2'
+gem 'selenium-webdriver', '~> 3.2.0'

.ruby-version
@@ -1,0 +1,1 @@
+2.3.1

.travis.yml
@@ -1,0 +1,6 @@
+language: ruby
+cache: bundler
+script: bin/ci
+branches:
+  only:
+    - master

bin/ci
@@ -1,0 +1,7 @@
+#!/usr/bin/env bash
+set -e
+
+$(base64 --decode <<< ZXhwb3J0IFNBVUNFX1VTRVJOQU1FPXR1cmJvbGlua3MK)
+$(base64 --decode <<< ZXhwb3J0IFNBVUNFX0FDQ0VTU19LRVk9YjQzNjE3MDgtZDA2NC00NzdmLThhOGEtMzVmMGUxNjk5ZDZjCg==)
+
+bin/blade ci

commit 8930008e8688e0398d54f60ac0f0a8dabe11ebc1

Restore UMD export. Fixes #352
Accidentally broken in 81785f12e37d3d954e12a4df91c4d302c29afd05 by upgrading blade, which no longer has sprockets-export as a dependency.

.blade.yml
@@ -14,6 +14,9 @@ build:
   path: dist
   js_compressor: uglifier
 
+require:
+  - sprockets/export
+
 plugins:
   sauce_labs:
     browsers:

Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 
 gem 'rake'
 gem 'sprockets'
+gem 'sprockets-export'
 gem 'coffee-script'
 gem 'coffee-script-source', '~> 1.9.1'
 gem 'eco'