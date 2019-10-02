e6ad32a32eb33518b80337e42701ba184a4afc95
Dom Christie <christiedom@gmail.com>
2019-10-02 09:20:04 +0100

    Fix default browser refresh behaviour after POST request.

--- src/history.ts
+++ src/history.ts
@@ -9,6 +9,8 @@ type HistoryMethod = (state: any, title: string, url?: string | null | undefined
 
 export class History {
   readonly delegate: HistoryDelegate
+  initialLocation!: Location
+  initialRestorationIdentifier!: string
   started = false
   pageLoaded = false
 
@@ -16,8 +18,11 @@ export class History {
     this.delegate = delegate
   }
 
-  start() {
+  start(location: Location, restorationIdentifier: string) {
     if (!this.started) {
+      this.initialLocation = location
+      this.initialRestorationIdentifier = restorationIdentifier
+
       addEventListener("popstate", this.onPopState, false)
       addEventListener("load", this.onPageLoad, false)
       this.started = true
@@ -44,10 +49,10 @@ export class History {
 
   onPopState = (event: PopStateEvent) => {
     if (this.shouldHandlePopState()) {
-      const { turbolinks } = event.state
-      if (turbolinks) {
-        const location = Location.currentLocation
-        const { restorationIdentifier } = turbolinks
+      const location = Location.currentLocation
+      const restorationIdentifier = this.restorationIdentifierForPopState(event)
+
+      if (restorationIdentifier) {
         this.delegate.historyPoppedToLocationWithRestorationIdentifier(location, restorationIdentifier)
       }
     }
@@ -68,6 +73,20 @@ export class History {
 
   pageIsLoaded() {
     return this.pageLoaded || document.readyState == "complete"
+  }
+
+  restorationIdentifierForPopState(event: PopStateEvent) {
+    if (event.state) {
+      return (event.state.turbolinks || {}).restorationIdentifier
+    }
+
+    if (this.poppedToInitialEntry(event)) {
+      return this.initialRestorationIdentifier
+    }
+  }
+
+  poppedToInitialEntry(event: PopStateEvent) {
+    return !event.state && Location.currentLocation.isEqualTo(this.initialLocation)
   }
 
   update(method: HistoryMethod, location: Location, restorationIdentifier: string) {
